5c5f2bb1e17a71a712d4c3cb3e7d54a20fca0539
wedesignstudios <deelden@gmail.com>
2018-01-09 14:30:20 -0600

    initial app set up

---
+++ public/app/App.js
@@ -0,0 +1,22 @@
+// React code starts here
+import React, { Component } from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Return To Now</h1>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(
+  (
+    <div>
+      <App />
+    </div>
+  ),
+  document.getElementById('app')
+);
